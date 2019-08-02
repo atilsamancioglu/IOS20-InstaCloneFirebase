6dd19a391dfd0c7986c5007675b41e8eeb24f7e9
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-08-02 16:14:20 +0300

    first commit

---
+++ InstaCloneFirebase/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  InstaCloneFirebase
+//
+//  Created by Atil Samancioglu on 1.08.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# IOS20-InstaCloneFirebase
